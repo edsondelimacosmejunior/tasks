a31e3687575398e5d0006b349ed8dc6d60064840
edsonjunior <edsondelimajunior@gmail.com>
2019-03-07 16:46:50 -0300

    First commit

---
+++ src/components/Task.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import moment from 'moment'
+import 'moment/locale/pt-br'
+import commonStyles from '../commonStyles'
+
+export default props => {
+    
+}
